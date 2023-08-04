f34dd1b5021b8b8a40a12fee312a896a353f50be
mbouanik <mbouanik@student.42.fr>
2023-08-04 14:13:11 -0500

    set up the app

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,13 @@
 # Put your app in here.
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "hello"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
