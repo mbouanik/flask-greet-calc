87ada4960b54062641d1bce4b95a40794fe39259
mbouanik <mbouanik@student.42.fr>
2023-08-04 14:36:27 -0500

    done

--- calc/app.py
+++ calc/app.py
@@ -1,5 +1,6 @@
 # Put your app in here.
-from flask import Flask
+from flask import Flask, request
+from operations import *
 
 app = Flask(__name__)
 
@@ -7,6 +8,34 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "hello"
+
+
+@app.route("/add")
+def addition():
+    a = request.args["a"]
+    b = request.args["b"]
+    return f"{add(int(a), int(b))}"
+
+
+@app.route("/sub")
+def substract():
+    a = request.args["a"]
+    b = request.args["b"]
+    return f"{sub(int(a), int(b))}"
+
+
+@app.route("/mult")
+def multiply():
+    a = request.args["a"]
+    b = request.args["b"]
+    return f"{mult(int(a), int(b))}"
+
+
+@app.route("/div")
+def divide():
+    a = request.args["a"]
+    b = request.args["b"]
+    return f"{div(int(a), int(b))}"
 
 
 if __name__ == "__main__":
